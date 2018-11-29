a639b28c5aef47deaabf0c3ea76a50197db21057
Hoàng Nhật Linh <hoangnhatlinh97@gmail.com>
2018-11-30 01:13:01 +0700

    ung dung chat room

---
+++ ROOM/public/xuly.js
@@ -0,0 +1,24 @@
+var socket = io("https://nhatlinhh.herokuapp.com");
+socket.on("server-send-dsrooms",function(data){
+    $("#dsRoom").html("");
+    data.map(function(r){
+        $("#dsRoom").append("<h4 class='room'>" + r + "</h4>");
+    })
+});
+
+socket.on("server-send-room-socket",function(data){
+    $("#roomHienTai").html(data);
+});
+
+socket.on("server-chat",function(data){
+    $("#right").append("<div>" + data +"</div>");
+});
+
+$(document).ready(function(){
+   $("#btnTaoRoom").click(function(){
+       socket.emit("tao-room",$("#txtRoom").val());
+   });
+   $("#btnChat").click(function(){
+       socket.emit("user-chat", $("#txtMessage").val());
+   });
+});
